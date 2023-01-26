7bf1c88b6469056632bb6e86abea63cb7acdc8b1
German Mazku <germanmazku@gmail.com>
2023-01-26 10:15:34 +0100

    update

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db.js')
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.query('SELECT * FROM users')
+  res.json(rows)
+});
+
+router.get('/ping', async function(req, res, next) {
+  const [result] = await pool.query('SELECT "Hello world" as RESULT')
+  //console.log(result[0])
+  res.json(result[0]);
+});
+
+router.get('/create', async function(req, res, next) {
+  const result = await pool.query('INSERT INTO users(name) VALUES ("Raiden")')
+  //console.log(result[0])
+  res.json(result);
+});
+
+module.exports = router;
